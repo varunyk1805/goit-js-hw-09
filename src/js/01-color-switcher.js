fcdd83d00592b343236ffe9c2bd0c1aa13c77b34
varunyk1805 <varunyk1805@gmail.com>
2021-11-25 14:07:35 +0200

    add compledet 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const backgroundColorBody = () => {
+    body.style.backgroundColor = getRandomHexColor();
+};
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(backgroundColorBody, 1000);
+    startBtn.setAttribute('disabled', 'true');
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+});
