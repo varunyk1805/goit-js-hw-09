c1734d553d25cb320814a8508c523f32cae5864a
varunyk1805 <varunyk1805@gmail.com>
2021-11-30 01:29:30 +0200

    add upgrade 02-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import { Ukrainian } from "flatpickr/dist/l10n/uk.js"
+import { Ukrainian } from "flatpickr/dist/l10n/uk.js";
 import Notiflix from 'notiflix';
 
 flatpickr.localize(Ukrainian);
@@ -14,34 +14,22 @@ Notiflix.Notify.init({
   fontSize: '16px'
 });
 
-const body = document.querySelector('body');
-const inputEl = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
 const daysEl = document.querySelector('span[data-days]');
 const hoursEl = document.querySelector('span[data-hours]');
 const minutesEl = document.querySelector('span[data-minutes]');
 const secondsEl = document.querySelector('span[data-seconds');
-const timerEl = document.querySelector('.timer');
 const fieldEl = document.querySelectorAll('.field');
 const valueEl = document.querySelectorAll('.value');
 const labelEl = document.querySelectorAll('.label');
-
-body.style.paddingLeft = '2vmin';
-
-inputEl.style.fontSize = '3vmin';
-
-startBtn.style.fontSize = '3vmin';
-startBtn.style.textTransform = 'uppercase';
-
-timerEl.style.display = 'flex';
-timerEl.style.marginTop = '3vmin';
+const resultEl = document.querySelector('.result');
 
 fieldEl.forEach(field => {
     field.style.display = 'flex';
     field.style.flexDirection = 'column';
     field.style.alignItems = 'center';
     field.style.marginLeft = '2vmin';
-})
+});
 
 fieldEl[0].style.marginLeft = '0';
 
@@ -54,15 +42,15 @@ labelEl.forEach(label => {
     label.style.fontSize = '3vmin';
     label.style.lineHeight = '1.1';
     label.style.textTransform = 'uppercase';
+});
 
-})
-
-let currentDate = new Date();
-let selectedDate = new Date();
+let currentUnixTime;
+let selectedUnixTime;
+let diffUnixTime;
+let tempUnixTime;
+let selectedDate;
 
 startBtn.setAttribute('disabled', 'true');
-
-let time = 0;
 
 const convertMs = ms => {
     const second = 1000;
@@ -76,7 +64,7 @@ const convertMs = ms => {
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
     
     return { days, hours, minutes, seconds };
-}
+};
 
 const options = {
     enableTime: true,
@@ -84,16 +72,16 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        selectedDate = selectedDates[0];
-        if (selectedDate.getTime() < currentDate.getTime()) {
+        currentUnixTime = new Date().getTime();
+        selectedUnixTime = selectedDates[0].getTime();
+        console.log(selectedDates[0]);
+        if (selectedUnixTime < currentUnixTime) {
             startBtn.setAttribute('disabled', 'true');
-            // window.alert("Please choose a date in the future")
             Notiflix.Notify.failure('Please choose a date in the future');
         } else {
             startBtn.removeAttribute('disabled');
-            time = selectedDate.getTime() - currentDate.getTime();
-            return time;
-        }
+            selectedDate = flatpickr.formatDate(selectedDates[0], "Y-m-d H:i");
+        };
     },
 };
 
@@ -102,27 +90,28 @@ flatpickr('#datetime-picker', options);
 const addLeadingZero = value => String(value).padStart(2, '0');
 
 const timer = () => {
-    const endTime = convertMs(time);
+    const endTime = convertMs(diffUnixTime);
 
     daysEl.textContent = addLeadingZero(endTime.days);
     hoursEl.textContent = addLeadingZero(endTime.hours);
     minutesEl.textContent = addLeadingZero(endTime.minutes);
     secondsEl.textContent = addLeadingZero(endTime.seconds);
-}
-
-let timerId = null;
+};
 
 const interval = () => {
+    resultEl.textContent = `Until ${selectedDate} left:`;
+    tempUnixTime = selectedUnixTime;
+    diffUnixTime = tempUnixTime - currentUnixTime;
+
+    let timerId = null;
     timerId = setInterval(() => {
-        if (time >= 1000) {
-            currentDate = new Date();
-            time = selectedDate.getTime() - currentDate.getTime();
+        if (diffUnixTime >= 1000) {
+            diffUnixTime = tempUnixTime - new Date().getTime();
             timer();
-            return time;
         } else {
             clearInterval(timerId);
             startBtn.removeEventListener('click', interval);
-        }
+        };
     }, 1000);
 };
 
